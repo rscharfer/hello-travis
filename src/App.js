16ce05e550b7ba1664d6a3a2a22cb04d4f048e4d
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2018-08-04 17:00:54 +0200

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default () => (
       <h1 className="App-title">Welcome To My Hello World!</h1>
     </header>
     <p className="App-intro">
-      Updating the travis file so that it will something to github using status api.
+      last change before I run. i promise.
     </p>
   </div>
 );
