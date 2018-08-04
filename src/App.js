ef26ce4dc053662c39fe50fa3a3762201a3ea21e
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2018-08-04 12:22:52 +0200

    rest of first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To My Hello World!</h1>
+    </header>
+    <p className="App-intro">
+      My name is Ryan. Welcome, and hello!
+    </p>
+  </div>
+);
+
