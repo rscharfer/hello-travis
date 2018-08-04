746b8bb106cf2c496a53fe17f45bb6e624e8351f
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2018-08-04 15:58:48 +0200

    updated travis config

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default () => (
       <h1 className="App-title">Welcome To My Hello World!</h1>
     </header>
     <p className="App-intro">
-      Travis, you are not going to delete the ENV variables I gave you are you?
+      Updating the travis file so that it will something to github using status api.
     </p>
   </div>
 );
